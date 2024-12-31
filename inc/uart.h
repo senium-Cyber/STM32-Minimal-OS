746aeca7d15364d4ee15fcddd1ee2a4b316511c8
TsipacsE <csxtchen@comp.hkbu.edu.hk>
2024-12-31 15:42:28 +0800

    Update uart.h

--- inc/uart.h
+++ inc/uart.h
@@ -9,6 +9,9 @@ void UART_Init(void);
 // UART 发送数据函数
 void UART_Send(const char *data);
 
+// UART 进行日志记录
+void UART_Log(const char *format, ...);
+
 // UART 接收数据回调
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 
