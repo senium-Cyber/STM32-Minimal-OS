ca74b032ab43c1c38c5ed37f6c80c7c12fc11f67
TsipacsE <csxtchen@comp.hkbu.edu.hk>
2024-12-31 15:28:40 +0800

    Create uart.h

---
+++ inc/uart.h
@@ -0,0 +1,15 @@
+#ifndef UART_H
+#define UART_H
+
+#include "stm32f4xx_hal.h"
+
+// UART 初始化函数
+void UART_Init(void);
+
+// UART 发送数据函数
+void UART_Send(const char *data);
+
+// UART 接收数据回调
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+
+#endif // UART_H
